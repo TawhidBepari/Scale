1180244e7635e68560ee79bde2ee7821ee23ece1
TawhidBepari <tawhidbepari2005@gmail.com>
2026-07-18 23:40:08 +0600

    Update admin-get-reviews.js

--- api/admin-get-reviews.js
+++ api/admin-get-reviews.js
@@ -41,11 +41,11 @@ console.log(
 
     if (error) {
 
-      return res.status(500).json({
-        error: error.message
-      });
+  console.error(error);
 
-    }
+  return res.status(500).json(error);
+
+}
 
     return res.status(200).json({
       success: true,
