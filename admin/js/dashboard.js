1e7649e390c0f20ea08a22fc39d1290196d8e128
TawhidBepari <tawhidbepari2005@gmail.com>
2026-07-19 22:58:05 +0600

    Create dashboard.js

---
+++ admin/js/dashboard.js
@@ -0,0 +1,39 @@
+const admin = localStorage.getItem("scale_admin");
+
+if(!admin){
+
+  window.location.href = "/admin/login.html";
+
+}
+
+document.getElementById("logoutBtn").onclick = () => {
+
+  localStorage.removeItem("scale_admin");
+
+  window.location.href = "/admin/login.html";
+
+};
+
+const menuButtons =
+  document.querySelectorAll(".menu button");
+
+const sections =
+  document.querySelectorAll(".section");
+
+menuButtons.forEach(button => {
+
+  button.onclick = () => {
+
+    const target = button.dataset.section;
+
+    sections.forEach(section => {
+      section.classList.remove("active");
+    });
+
+    document
+      .getElementById(target)
+      .classList.add("active");
+
+  };
+
+});
