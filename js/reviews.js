ff9ba5445ac46500b69f6849ae8acd56c2ff974a
TawhidBepari <tawhidbepari2005@gmail.com>
2026-08-03 22:02:51 +0600

    Update reviews.js

--- js/reviews.js
+++ js/reviews.js
@@ -1,1 +1,72 @@
+async function loadReviews(){
 
+  const { data, error } = await supabaseClient
+    .from("reviews")
+  .select(`
+    *,
+    review_replies (
+      reply
+    )
+  `)
+  .eq("active", true);
+  
+  if(error){
+
+    console.error(error);
+
+    return;
+  }
+
+console.log(data);
+  
+  const container =
+    document.getElementById("reviewsContainer");
+
+  container.innerHTML = "";
+
+  if(!data.length){
+
+    container.innerHTML = `
+      <div class="review">
+        <p>
+          No reviews yet.
+        </p>
+      </div>
+    `;
+
+    return;
+  }
+
+  data.forEach(review => {
+
+    const card = document.createElement("div");
+
+    card.className = "review";
+
+    card.innerHTML = `
+
+<div class="review-stars">
+
+${"★".repeat(review.rating || 5)}
+
+</div>
+
+<p class="review-text">
+
+"${review.review || ""}"
+
+</p>
+
+<strong>
+
+${review.client_name || "Anonymous"}
+
+</strong>
+
+`;
+
+    container.appendChild(card);
+
+  });
+
+}
